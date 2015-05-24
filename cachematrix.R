9cce27d0b1595e9db6029429baee0e5c187bde35
Schneider Eusse <eusse234@gmail.com>
2015-05-24 11:45:48 +0000

    Modification of cachematrix.R for Programming Assignment 2. R Programming Course (rprog-014)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function ("MakeCacheMAtrix") create a "speial" list containing a 
+## matrix and caclulate its inverse using the solve function. Then the 
+## "cacheSolve" function solve the inverse of the matrix set in "makeCacheMatrix"
+# but first check if its inverse is already calculated into the same 
+## "makeCacheMatrix" function and skip the computation.
 
-## Write a short comment describing this function
+## This function create a list containing 1) set the matrix, 2) get the matrix,
+## 3) set the inverse of the matrix and 4) get the inverse of the matrix. the input
+## argument will be a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    setmatrix <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    getmatrix <- function() x
+    setinverse <- function(solve) inv <<- solve
+    getinverse <- function() inv
+    list(set = setmatrix, get = getmatrix,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+## This function check if the inverse of the matrix setted in list$set
+## is already been calculated in list$getinverse. If it is true the function take 
+## the value from the cache, otherwise, it calculate the inverse of the data and 
+## set its value in the cache via "setinverse" function.
 
-## Write a short comment describing this function
+cacheSolve <- function(x=matrix(), ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix, ...)
+    x$setinverse(inv)
+    inv
+} 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Return a matrix that is the inverse of 'x'
